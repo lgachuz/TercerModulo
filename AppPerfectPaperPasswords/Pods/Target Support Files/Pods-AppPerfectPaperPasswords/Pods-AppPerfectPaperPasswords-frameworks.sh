75bd5df724928fb44c6dd29c1f34f0d8ef27edda
Luis Gachuz <secmiserio29@hotmail.com>
2020-02-05 17:24:06 -0600

    Agregar Pod

--- AppPerfectPaperPasswords/Pods/Target Support Files/Pods-AppPerfectPaperPasswords/Pods-AppPerfectPaperPasswords-frameworks.sh
+++ AppPerfectPaperPasswords/Pods/Target Support Files/Pods-AppPerfectPaperPasswords/Pods-AppPerfectPaperPasswords-frameworks.sh
@@ -161,9 +161,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/RijndaelSwift/RijndaelSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/UInt128/UInt128.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/RijndaelSwift/RijndaelSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/UInt128/UInt128.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
